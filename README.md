14eba992e9901e51b6a1f6e0da4312ccab485796
quantumofcosmos <quantumofcosmos@gmail.com>
2019-06-25 12:26:22 +0530

    updated readme and transpose logic

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Western-to-classical-note-convertor
-An attempt to convert western music written in staff notation in A B C to carnatic (Indian classical) music notation in Sa Ri Ga
+An attempt to convert western music written in A B C to carnatic (Indian classical) music notation in Sa Ri Ga.
+
+script abc2srg takes in a text file with western notation(Only notations without lyrics), takes a value to transpose the notes if required(currently at 0 in script)
+
+Gives out a text file with carnatic notations as out.txt
+
+*ToD0:* Fix issues with transpose morethan 1.
--- abc2srg.py
+++ abc2srg.py
@@ -7,12 +7,13 @@
 western = open("in.txt","r")
 carnatic = open("out.txt","w")
 
-ASD={"E":"1","F":"2","F#":"3","G":"4","G#":"5","A":"6","A#":"7","B":"8","C":"9","C#":"10","D":"11","D#":"12"}
-SRG={"1":"S","2":"R1","3":"R2","4":"G1","5":"G2","6":"M1","7":"M2","8":"P","9":"D1","10":"D2","11":"N1","12":"N2"}
+ASD={"e":"1","f":"2","f#":"3","g":"4","g#":"5","a":"6","a#":"7","b":"8","c":"9","c#":"10","d":"11","d#":"12","E":"13","F":"14","F#":"15","G":"16","G#":"17","A":"18","A#":"19","B":"20","C":"21","C#":"22","D":"23","D#":"24"}
+SRG={"1":"s","2":"r1","3":"r2","4":"g1","5":"g2","6":"m1","7":"m2","8":"p","9":"d1","10":"d2","11":"n1","12":"n2","13":"S","14":"R1","15":"R2","16":"G1","17":"G2","18":"M1","19":"M2","20":"P","21":"D1","22":"D2","23":"N1","24":"N2","25":"S+"}
 
 def convertor(note):
     sharp="F"
     final=""
+    transpose = 0
     for index,alpha in enumerate(note):
         if sharp=="T":
             sharp="F"
@@ -20,11 +21,12 @@ def convertor(note):
             try:
                 if(note[index+1]=="#"):
 #                     print(SRG[ASD[alpha+"#"]])
-                    final=final+str(SRG[ASD[alpha+"#"]])+" "
+#                    print(str(int(ASD[alpha+"#"])+transpose))
+                    final=final+str(SRG[str(int(ASD[alpha+"#"])+transpose)])+" "
                     sharp="T"
                 else:
 #                     print(SRG[ASD[alpha]])
-                    final=final+SRG[ASD[alpha]]+" "
+                    final=final+SRG[str(int(ASD[alpha])+transpose)]+" "
             except:
                 continue
     return(final)
