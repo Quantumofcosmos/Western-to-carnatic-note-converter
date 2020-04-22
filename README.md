6166d11db9ff4226e1330c587362774f4b118e74
quantumofcosmos <quantumofcosmos@gmail.com>
2020-04-22 22:09:45 +0530

    now with a simpler logic

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Western-to-classical-note-convertor
-An attempt to convert western music written in A B C to carnatic (Indian classical) music notation in Sa Ri Ga.
+An attempt to convert western music notes written in A B C to carnatic (Indian classical) music notation in Sa Ri Ga.
 
-script abc2srg takes in a text file with western notation(Only notations without lyrics), takes a value to transpose the notes if required(currently at 0 in script)
+![octaves of C](/future/octaves_of_C.png)
 
-Gives out a text file with carnatic notations as out.txt
+The script abc2srg takes in a text file with western notation(with  appropriate octaves) and translates them to a text file with carnatic notations (out.txt) at 3 kattai(E is Sa).
 
-*ToD0:* Fix issues with transpose morethan 1.
+
+Parameters being passed are name of the input file and desired pitch shift (conversion will be irrespective of scale)
+
+sample call:
+
+```
+python3 abc2srg.py
+```
+Converts file in.txt to out.txt with 0 pitch shift.
+
+Sample Conversion:
+
+```E3  F3  F3#``` gets converted to ```Ṣ Ṛ1 Ṛ2```
--- future/abc2srg.py
+++ future/abc2srg.py
@@ -4,26 +4,7 @@
 ##ToDo:
 # implement transpose that can come around
 
-import sys
-
-scale = 0
-in_file = "in.txt"
-
-if len(sys.argv) == 2:
-	try:
-		scale = int(sys.argv[1])
-	except:
-		in_file = sys.argv[1]
-
-if len(sys.argv) == 3:
-	try:
-		scale = int(sys.argv[2])
-		in_file = sys.argv[1]
-	except:
-		scale = int(sys.argv[1])
-		in_file = sys.argv[2]
-
-western = open(in_file, "r")
+western = open("in.txt","r")
 carnatic = open("out.txt","w", encoding='utf-16')
 
 #ASD={"e":"1","f":"2","f#":"3","g":"4","g#":"5","a":"6","a#":"7","b":"8","c":"9","c#":"10","d":"11","d#":"12","E":"13","F":"14","F#":"15","G":"16","G#":"17","A":"18","A#":"19","B":"20","C":"21","C#":"22","D":"23","D#":"24"}
@@ -33,17 +14,34 @@ SRG={"1":"Ṣ","2":"Ṛ1","3":"Ṛ2","4":"G̣1","5":"G̣2","6":"Ṃ1","7":"Ṃ2"
 ASD={"E3":"1","F3":"2","F3#":"3","G3":"4","G3#":"5","A3":"6","A3#":"7","B3":"8","C4":"9","C4#":"10","D4":"11","D4#":"12","E4":"13","F4":"14","F4#":"15","G4":"16","G4#":"17","A4":"18","A4#":"19","B4":"20","C5":"21","C5#":"22","D5":"23","D5#":"24",
 			"E5":"25","F5":"26","F5#":"27","G5":"28","G5#":"29","A5":"30","A5#":"31","B5":"32","C6":"33","C6#":"34","D6":"35","D6#":"36"}
 
+TEST={"E3":"Ṣ","F3":"Ṛ1","F3#":"Ṛ2","G3":"G̣1","G3#":"G̣2","A3":"Ṃ1","A3#":"Ṃ2","B3":"P̣","C4":"Ḍ1","C4#":"Ḍ","D4":"11","D4#":"12","E4":"13","F4":"14","F4#":"15","G4":"16","G4#":"17","A4":"18","A4#":"19","B4":"20","C5":"21","C5#":"22","D5":"23","D5#":"24"}
+
+def convertor(note):
+    sharp="F"
+    final=""
+    transpose = 0
+    for index,alpha in enumerate(note):
+        if sharp=="T":
+            sharp="F"
+        else:
+            try:
+                if(note[index+1]=="#"):
+#                     print(SRG[ASD[alpha+"#"]])
+#                    print(str(int(ASD[alpha+"#"])+transpose))
+                    final=final+str(SRG[str(int(ASD[alpha+"#"])+transpose)])+" "
+                    sharp="T"
+                else:
+#                     print(SRG[ASD[alpha]])
+                    final=final+SRG[str(int(ASD[alpha])+transpose)]+" "
+            except:
+                continue
+    return(final)
 
 for line in western:
-	one_line=""
-	for each in line.split():
-		carnatic.write(SRG[str(int(ASD[each])+scale)])
-		carnatic.write(" ")
-	carnatic.write("\n")
-
+    thisline = convertor(line)
+    carnatic.write(thisline + '\n')
 western.close()
 carnatic.close()
-
 
 
 #S	R1	R2/G1	R3/G2	    G3	  M1	M2	 P	D1	D2/N1	D3/N2	N3
